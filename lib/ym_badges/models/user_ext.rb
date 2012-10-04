9bf4af84903330128ca9f334194bb3410233bda7
Edward Andrews <edward@yoomee.com>
2012-10-04 15:26:34 +0100

    Allow multiple awards of the same badge

--- lib/ym_badges/models/user_ext.rb
+++ lib/ym_badges/models/user_ext.rb
@@ -2,12 +2,12 @@ module YmBadges::UserExt
 
   def self.included(base)
     base.has_many(:badge_awardings, :class_name => "YmBadges::BadgeAwarding", :dependent => :destroy)
-    base.has_many(:badges, :through => :badge_awardings)
+    base.has_many(:badges, :through => :badge_awardings, :class_name => "Badge")
   end
 
   def award_badge!(slug)
     return true if !Settings.allow_multiple_awardings_of_badge && badges.exists?(:slug => slug)
-    if badge = Badge.find_by_slug(slug.to_s)
+    if badge = ::Badge.find_by_slug(slug.to_s)
       self.badges << badge
     end
   end
