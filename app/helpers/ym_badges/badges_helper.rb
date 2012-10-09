efdc9359cad324c0e39392647a403a25d8ee85e4
Edward Andrews <edward@yoomee.com>
2012-10-09 12:20:00 +0100

    Show one modal with all badges in it

--- app/helpers/ym_badges/badges_helper.rb
+++ app/helpers/ym_badges/badges_helper.rb
@@ -1,15 +1,14 @@
 module YmBadges::BadgesHelper
-  
+
   def render_new_badge_modal
     return '' if logged_out?
-    if (badge_awardings = current_user.badge_awardings.where(:notified => false)).present?
-      "".tap do |out|
-        badge_awardings.each do |badge_awarding|
-          badge_awarding.update_attribute(:notified, true)
-          out << render('badges/modal', :badge => badge_awarding.badge)
-        end
-      end.html_safe
+    # if (badge_awardings = current_user.badge_awardings.where(:notified => false)).present?
+    if (badge_awardings = current_user.badge_awardings).present?
+      badge_awardings.update_all("notified = true")
+      badges = badge_awardings.collect(&:badge).uniq
+      render('badges/modal', :badges => badges)
+
     end
   end
-  
+
 end
