f97eb09956e9cd6dd17e6529139053e883b6561f
Edward Andrews <edward@yoomee.com>
2012-10-09 15:51:03 +0100

    Badge awarding works with only one badge

--- app/helpers/ym_badges/badges_helper.rb
+++ app/helpers/ym_badges/badges_helper.rb
@@ -3,11 +3,9 @@ module YmBadges::BadgesHelper
   def render_new_badge_modal
     return '' if logged_out?
     if (badge_awardings = current_user.badge_awardings.where(:notified => false)).present?
-    # if (badge_awardings = current_user.badge_awardings).present?
-      badge_awardings.update_all("notified = true")
       badges = badge_awardings.collect(&:badge).uniq
-      render('badges/modal', :badges => badges)
-
+      badge_awardings.update_all("notified = true")
+      (render 'badges/modal', :badges => badges).html_safe
     end
   end
 
