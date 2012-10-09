ca4dc0c22c4571f01864943c96d36eb92374ecac
Edward Andrews <edward@yoomee.com>
2012-10-09 13:32:19 +0100

    Oops left all badges notifying

--- app/helpers/ym_badges/badges_helper.rb
+++ app/helpers/ym_badges/badges_helper.rb
@@ -2,8 +2,8 @@ module YmBadges::BadgesHelper
 
   def render_new_badge_modal
     return '' if logged_out?
-    # if (badge_awardings = current_user.badge_awardings.where(:notified => false)).present?
-    if (badge_awardings = current_user.badge_awardings).present?
+    if (badge_awardings = current_user.badge_awardings.where(:notified => false)).present?
+    # if (badge_awardings = current_user.badge_awardings).present?
       badge_awardings.update_all("notified = true")
       badges = badge_awardings.collect(&:badge).uniq
       render('badges/modal', :badges => badges)
